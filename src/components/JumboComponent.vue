d34965b5db047045b5d91c77a8e965e39ec72a8d
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-11-11 16:21:10 +0100

    end main start bonus

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hero">
-
+        <button><a href="#nogo">current series</a> </button>
     </div>
 </template>
 
@@ -11,10 +11,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/style/partials/mixins' as *;
+@use '../assets/style/partials/variabels' as *;
+
 .hero {
     background-image: url(../../public/img/jumbotron.jpg);
     background-repeat: no-repeat;
     background-size: cover;
     height: 40vh;
+    position: relative;
+
+    button {
+        position: absolute;
+        bottom: -20px;
+        left: 210px;
+        background-color: $bluelink;
+        height: 40px;
+        padding: 10px;
+        border: none;
+        color: white;
+        margin-top: 10px;
+        ;
+
+        a {
+            text-transform: uppercase;
+            color: $whitetext;
+            font-size: 1.2em;
+            text-decoration: none;
+            font-weight: bold;
+        }
+    }
 }
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,19 +2,43 @@
     <main>
         <section>
             <div class="container-start">
-                <button><a href="#nogo"> current series </a> </button>
-                <h2> --> Contenctgoes Here &lt;-- </h2>
+
+                <div class="row">
+
+                    <div class="card" v-for="(comic, index) in comics" :key="index">
+                        <div class="imgbox">
+                            <img :src="comic.thumb" :alt="comic.series">
+                        </div>
+                        <div>
+                            <h4>{{ comic.series }}</h4>
+
+                        </div>
+                    </div>
+
+
+                </div>
 
             </div>
 
         </section>
+        <div class="btn">
+            <button>load more</button>
+        </div>
 
     </main>
 </template>
 
 <script>
+import { comics } from '../data/data';
 export default {
-    name: 'MainComponent'
+    name: 'MainComponent',
+    data() {
+        return {
+            comics: comics
+        }
+    }
+
+
 
 }
 </script>
@@ -24,47 +48,98 @@ export default {
 @use '../assets/style/partials/variabels' as *;
 
 main {
+
     color: $whitetext;
     background-color: $blacktext;
-}
+    padding-bottom: 20px;
 
-section {
-    @include dflex;
-    @include container;
-    background-color: $blacktext;
-    height: 15vh;
+    section {
+        @include dflex;
+        @include container;
+        background-color: $blacktext;
 
-    button {
-        position: absolute;
-        top: -50px;
-        left: 0;
-        background-color: $bluelink;
-        height: 40px;
-        padding: 10px;
-        border: none;
 
-        a {
-            text-transform: uppercase;
-            color: $whitetext;
-            font-size: 1.2em;
-            text-decoration: none;
-            font-weight: bold;
+
+
+
+
+        .container-start {
+            max-width: 1170px;
+            padding: 1rem;
+            display: flex;
+            justify-content: flex-start;
+
+            .row {
+
+                display: flex;
+                flex-flow: row wrap;
+
+
+
+                .card {
+                    // @include dflex;
+                    height: 210px;
+                    padding: 10px;
+                    width: calc(100% / 6);
+
+                    .imgbox {
+                        height: 90%;
+                        overflow-y: hidden;
+
+                        img {
+                            width: 90;
+
+                        }
+
+
+                    }
+
+                    div {
+                        h4 {
+                            text-align: center;
+                        }
+                    }
+
+
+
+                }
+
+
+
+
+                h2 {
+                    color: $whitetext;
+                }
+            }
         }
+
+
+
+
     }
 
+    .btn {
 
+        text-align: center;
+        margin-bottom: 10px;
 
-    .container-start {
-        max-width: 1170px;
-        padding: 1rem;
-        position: relative;
-
-        justify-content: flex-start;
-
-
-
-        h2 {
+        button {
+            background-color: $bluelink;
+            height: 40px;
+            width: 150px;
+            padding: 10px;
+            border: none;
+            margin-bottom: 10px;
+            text-transform: uppercase;
+            font-weight: bold;
             color: $whitetext;
+            font-size: 1.2em;
+
+            &:hover {
+                background-color: black;
+                border: 3px solid $bluelink;
+                padding: 10px;
+            }
         }
     }
 }
