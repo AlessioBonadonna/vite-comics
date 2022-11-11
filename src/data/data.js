079962ff4a54e5d2807a4efd0ba3d49c87a9fe44
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-11-11 17:22:50 +0100

    add [A] list for footer

--- src/data/data.js
+++ src/data/data.js
@@ -72,4 +72,53 @@ const comics = [
     "type": "graphic novel"
   }
 ]
-export { comics }
+const list = [
+  {
+    title: "dc comics",
+    list: [
+      "characters",
+      "comics",
+      "movies",
+      "tv",
+      "games",
+      "videos",
+      "news"
+    ]
+  },
+  {
+    title: "shop",
+    list: [
+      "shop dc",
+      "shop dc collectibles"
+    ]
+  },
+  {
+    title: "dc",
+    list: [
+      "terms of use",
+      "Privacy policy(new)",
+      "Ad choices",
+      "Advertising",
+      "Jobs",
+      "Subscriptions",
+      "Talent Workshops",
+      "CPSC certificate",
+      "Ratings",
+      "Shop help",
+      "Contact Us"
+    ]
+  },
+  {
+    title: "Sites",
+    list: [
+      "DC",
+      "MAD Magazine",
+      "DC Kids",
+      "DC Universe",
+      "DC Power Visa"
+    ]
+  }
+]
+
+
+export { comics, list }
